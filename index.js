291377d382363349abc24a9ed73e0a474d381844
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-08-03 16:02:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 const returnFirstTwoDrivers = function(arr){ return [arr[0], arr[1]]};
 const returnLastTwoDrivers = function(arr){ return [arr[arr.length - 2], arr[arr.length - 1]]};
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+const createFareMultiplier = function(int) {
+  return function(fare){
+    return fare * int;
+  }
+}
